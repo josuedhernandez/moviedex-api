7ec73e326dc660a57719d31c660fe0c10cac7419
Josue Hernandez <moradel82@gmail.com>
2020-11-09 02:36:40 -0700

    Clean up

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require('cors');
 const helmet = require('helmet');
-const MOVIEDEX = require("./movidex.json");
+const MOVIEDEX = require("./moviedex.js");
 
 const app = express();
 
