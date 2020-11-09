5d3ca7dc5a34f5a6632a9a4040654c33c1a08834
Josue Hernandez <moradel82@gmail.com>
2020-11-09 03:06:53 -0700

    Handling data in json format directly

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-const MOVIEDEX = require("./moviedex.js");
+const MOVIEDEX = require("./moviedex.json");
 
 const app = express();
 
