fda57ef4cbe407902c3c36c249b564bd5e66203b
Josue Hernandez <moradel82@gmail.com>
2020-11-11 00:31:36 -0700

    Preparing app for Heroku

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const MOVIEDEX = require("./moviedex.json");
 
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-  console.log("validate bearer token middleware");
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
@@ -49,7 +49,6 @@ function handleGetMovie(req, res) {
 
 app.get("/movie", handleGetMovie);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
 });
